246309c85ff758829330aede01b1c223122c61ca
Jonathan Nilsson <jonathan@voysys.se>
2025-12-19 14:44:30 +0100

    Day1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -37,15 +37,62 @@ pub fn a(input: &str) -> i32 {
 #[test]
 fn test_a() {
     assert_eq!(a(TEST_INPUT), 3);
-    assert_eq!(a(INPUT), 0);
+    assert_eq!(a(INPUT), 1118);
 }
 
 pub fn b(input: &str) -> i32 {
-    0
+    let mut dial = 50;
+    let mut zero_count = 0;
+
+    for line in input.lines() {
+        let line_b = line.as_bytes();
+
+        let amt = (str::from_utf8(&line_b[1..]).unwrap())
+            .parse::<i32>()
+            .unwrap();
+
+        let mut rotated = false;
+
+        let started_as_zero = dial == 0;
+
+        let full = amt / 100;
+        let rest = amt % 100;
+        zero_count += full;
+
+        match line_b[0] {
+            b'L' => dial -= rest,
+            b'R' => dial += rest,
+            _ => (),
+        };
+
+        if dial > 99 {
+            if !started_as_zero {
+                rotated = true;
+                zero_count += 1;
+            }
+
+            dial -= 100;
+        }
+
+        if dial < 0 {
+            if !started_as_zero {
+                rotated = true;
+                zero_count += 1;
+            }
+
+            dial += 100;
+        }
+
+        if dial == 0 && !rotated {
+            zero_count += 1;
+        }
+    }
+
+    zero_count
 }
 
 #[test]
 fn test_b() {
-    assert_eq!(b(TEST_INPUT), 0);
-    assert_eq!(b(INPUT), 0);
+    assert_eq!(b(TEST_INPUT), 6);
+    assert_eq!(b(INPUT), 6289);
 }
