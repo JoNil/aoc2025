bfed344a4160fd339adc97ca48b72701d6667cb9
Jonathan Nilsson <jonathan@voysys.se>
2025-12-19 14:45:44 +0100

    2

---
+++ src/days/day02.rs
@@ -0,0 +1,22 @@
+pub static INPUT: &str = include_str!("../input/2.txt");
+pub static TEST_INPUT: &str = include_str!("../input/2_test.txt");
+
+pub fn a(input: &str) -> i32 {
+    0
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(a(TEST_INPUT), 0);
+    assert_eq!(a(INPUT), 0);
+}
+
+pub fn b(input: &str) -> i32 {
+    0
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(b(TEST_INPUT), 0);
+    assert_eq!(b(INPUT), 0);
+}
--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,9 @@ fn main() {
     time(day01::a, day01::INPUT);
     time(day01::b, day01::INPUT);
 
+    time(day02::a, day02::INPUT);
+    time(day02::b, day02::INPUT);
+
     let elapsed = start.elapsed();
 
     println!("Total Time {} us", elapsed.as_micros());
